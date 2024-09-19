7a0d2ac17f06cf563783f4fc885c61c8d9afe4f4
John Thompson <john@springframework.guru>
2024-09-19 15:14:13 -0400

    Updating to new config

---
+++ cfg/postgres/initdb/setup.sql
@@ -0,0 +1,5 @@
+create user dataintake with password 'dataintake';
+create database dataintake with owner dataintake;
+
+create user keystore with password 'keystore';
+create database keystore with owner keystore;
